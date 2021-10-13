021a8188660a7dd855cb418eec7da121f2562093
james-2001 <james.a.may.2001@gmail.com>
2021-10-13 11:29:20 +0100

    correlation bootstrap and confidence intervals

--- ERvsCC.r
+++ ERvsCC.r
@@ -1,1 +1,11 @@
-data<-read.csv("data/ercc.csv")
+library(boot)
+
+trends <- read.csv("data/ercc.csv")
+
+cor.stat <- function(data, i) {
+  cor(data[i,1], data[i,2])
+}
+
+bs.trend <- boot(trends, cor.stat, R = 5000)
+hist(bs.trend$t)
+bs.ci <- boot.ci(bs.trend, type = "perc")
