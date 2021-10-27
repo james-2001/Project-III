f0f512f3c15bc8587a54c4b0983ba6cd86bf2149
james-2001 <james.a.may.2001@gmail.com>
2021-10-27 21:23:23 +0100

    cis in python

--- python/bootstrap.py
+++ python/bootstrap.py
@@ -5,4 +5,4 @@ import numpy as np
 x = [random() for _ in range(10)]
 
 bs = sp.bootstrap((x,), np.mean)
-print((bs))
+print(bs)
---
+++ python/bs_sk.py
@@ -0,0 +1,24 @@
+import random
+from numpy.core.fromnumeric import sort
+import sklearn.utils as sk
+import numpy as np
+import scipy.stats as sp
+
+data = random.sample(range(1000),100)
+
+mu = np.mean(data)
+
+R = 5000
+bootstrap = np.array([np.mean(sk.resample(data))
+                      for _ in range(R)])
+
+bs_basic_py = sp.bootstrap((data,), np.mean, 
+             method = 'basic').confidence_interval
+print(bs_basic_py)
+
+bs_quantiles = sort(bootstrap)[[round(R*0.025),round(R*0.975)]]
+bs_basic_me = 2*mu - bs_quantiles
+print(sorted(bs_basic_me))
+
+
+
