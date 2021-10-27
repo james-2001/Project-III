6186b5a42a85596adae90b0301be1c5fb4bc1fa8
james-2001 <james.a.may.2001@gmail.com>
2021-10-27 20:12:55 +0100

    percentile cis

--- ERvsCC.r
+++ ERvsCC.r
@@ -17,12 +17,17 @@ bs.ci.perc1 <- boot.ci(bs, type = "perc")
 hist(bs$t)
 abline(v = bs.ci.norm1$normal[c(2,3)], col = 3)
 abline(v = bs.ci.basic1$basic[c(4,5)], col = 2)
+abline(v = bs.ci.perc1$percent[c(4,5)], col = 4)
 
 bs.bias <- mean(bs$t - bs$t0)
 bs.std <- var(bs$t)**0.5
-
 bs.ci.norm2 <- c(bs$t0 - bs.bias - 1.96*bs.std, bs$t0 - bs.bias + 1.96*bs.std)
 
 bs.sorted <- sort(bs$t)
 bs.quantiles <- c(bs.sorted[round(r*0.975)], bs.sorted[round(r*0.025)])
 bs.ci.basic2 <- 2*bs$t0 - bs.quantiles
+
+bs.ci.perc2 <- sort(bs.quantiles)
+# bs bias is small so might be a reasonable assumption
+
+
