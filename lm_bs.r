313aa319dc29eff6e447c7f8bfe9b5d1827f6c85
james-2001 <james.a.may.2001@gmail.com>
2021-11-02 16:43:30 +0000

    bs on residual error of lr

--- lm_bs.r
+++ lm_bs.r
@@ -2,11 +2,17 @@ library(boot)
 
 male_cats <- catsM[,c(2,3)]
 model <- lm(Hwt~Bwt, data = male_cats)
+plot(male_cats)
 abline(model$coefficients[1], model$coefficients[2])
 
+residual_std_error <- function(data, i){
+  d <- data[i]
+  ((sum(d**2))/(length(d) - 2))**0.5
+  
+}
 
-residual_boot <- boot(model$residuals,statistic = function(data,i){mean(data[i])},
+
+residual_boot <- boot(model$residuals, statistic = residual_std_error,
                       R = 5000)
 
 residual_boot.ci <- boot.ci(residual_boot, type = "norm")
-# symmetric about 0: evidence residuals are normally distrubutes
