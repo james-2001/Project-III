97b8daa79d68a8064aa8139d823d2db308e54a26
james-2001 <james.a.may.2001@gmail.com>
2021-11-10 12:02:54 +0000

    bootstrap lr for ducks

--- lm_bs.r
+++ lm_bs.r
@@ -1,8 +1,7 @@
 library(boot)
 
-male_cats <- catsM[,c(2,3)]
-model <- lm(Hwt~Bwt, data = male_cats)
-plot(male_cats)
+model <- lm(behaviour~plumage, data = ducks)
+plot(ducks)
 abline(model$coefficients[1], model$coefficients[2])
 
 residual_std_error <- function(data, i){
@@ -12,7 +11,25 @@ residual_std_error <- function(data, i){
 }
 
 
+model_intercept <- function(data, i){
+  d <- data[i,]
+  md <- lm(behaviour ~ plumage, data = d)
+  md$coefficients[1]
+}
+
+
+model_grad <- function(data, i){
+  d <- data[i,]
+  md <- lm(behaviour ~ plumage, data = d)
+  md$coefficients[2]
+}
+
 residual_boot <- boot(model$residuals, statistic = residual_std_error,
                       R = 5000)
 
 residual_boot.ci <- boot.ci(residual_boot, type = "norm")
+
+
+
+duck_resample_grad <- boot(ducks,model_grad, R = 5000)
+duck_resample_intercept <- boot(ducks,model_intercept, R = 5000)
