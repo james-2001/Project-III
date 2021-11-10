3b05fa5e820973fa9d4d15d12ab22e7d8f881ca8
james-2001 <james.a.may.2001@gmail.com>
2021-11-10 15:40:59 +0000

    add plots

--- lm_bs.r
+++ lm_bs.r
@@ -1,9 +1,11 @@
 library(boot)
 
 model <- lm(plumage~behaviour, data = ducks)
-plot(ducks, ylim = c(-3, 20))
-abline(model$coefficients, col = 3)
 
+plot(ducks, ylim = c(-3, 20))
+abline(model$coefficients, col = "black")
+abline(confint(model)[,1], col = "blue")
+abline(confint(model)[,2], col = "blue")
 
 model_intercept <- function(data, i){
   d <- data[i,]
@@ -24,8 +26,7 @@ duck_grad.ci <- boot.ci(duck_grad, type = 'perc')
 duck_intercept <- boot(ducks,model_intercept, R = 5000)
 duck_intercept.ci <- boot.ci(duck_intercept, type = 'perc')
 
-abline(duck_intercept$t0, duck_grad$t0, col = 2, lty = 2)
-abline(duck_intercept.ci$percent[4], duck_grad.ci$percent[4], col = 5)
-abline(duck_intercept.ci$percent[5], duck_grad.ci$percent[5], col = 5)
+abline(duck_intercept$t0, duck_grad$t0, col = "red", lty = "dashed")
 
-
+abline(duck_intercept.ci$percent[4], duck_grad.ci$percent[4], col = "green")
+abline(duck_intercept.ci$percent[5], duck_grad.ci$percent[5], col = "green")
