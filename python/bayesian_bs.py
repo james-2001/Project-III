e1de34b4492cbe7cf3a74217687795180b041003
james-2001 <james.a.may.2001@gmail.com>
2021-11-16 09:27:06 +0000

    correct bb for mean

--- python/bayesian_bs.py
+++ python/bayesian_bs.py
@@ -1,24 +1,31 @@
 import numpy as np
+from numpy.lib.function_base import quantile
 import scipy as sp
 import sklearn.utils as sk
 
 
 class BB:
-    def __init__(self, input_data, R) -> None:
+    def __init__(self, input_data: list, R: int) -> None:
         self.data = np.array(input_data)
         self.R = R
         self.t0 = np.mean(input_data)
-        self.t = [None]
         self.n = len(input_data)
+        self.t = self.b_bootstrap()
+        
 
-
-    def bs_average(self):
-        dir_samples = np.random.dirichlet([1]*self.n, self.R)
-        self.t = (dir_samples * self.data).sum(axis=1)
+    def b_bootstrap(self):
+        dir_samp = np.random.dirichlet([1]*self.n, self.R)
+        t = [None] * self.R
+        for i in range(self.R):
+            t[i] = np.average(self.data, weights=dir_samp[i])
+        return t 
+ 
 
 
 if __name__ == "__main__":
-    norm_data = np.random.normal(0,1,30)
+    norm_data = np.random.normal(0,1,1000)
     bs = BB(norm_data, 5000)
+    print(bs.t0)
+    print(np.quantile(bs.t, [0.025,0.975]))
 
 
