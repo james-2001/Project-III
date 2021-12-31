8d292c5d580143ef31987532051828685ca49bcb
james-2001 <james.a.may.2001@gmail.com>
2021-12-31 16:05:57 +0000

    acme comparison

--- scratch.r
+++ scratch.r
@@ -1,22 +1,19 @@
 library(boot)
 set.seed(5)
 
-
-boot1 <- function(data, n) {
-  correlations <- integer(n)
-  for (i in 1:n) {
-    sample_index <- sample(1:14, 5, replace = T)
-    resample <- law[sample_index,]
-    resample.cor <- cor(resample$LSAT, resample$GPA)
-    correlations[i] <- resample.cor
+boot1 <- function(R) {
+  t <- rep(0, R)
+  for (i in 1:R) {
+    resample <- acme[sample(nrow(acme), nrow(acme), replace = T), ]
+    t[i] <- cor(resample$market, resample$acme)
   }
-  mean(correlations)
+  return(t)
 }
 
-statistic.me <- boot1(law, 1000)
+boot_scratch <- boot1(law, 5000)
 
 theta_hat <- function(data, index){
   return(cor(data[index,1], data[index,2]))
 }
 
-statistic.r <- boot(law, theta_hat, R = 5000)$t0
+boot_library <- boot(law, theta_hat, R = 5000)$t0
