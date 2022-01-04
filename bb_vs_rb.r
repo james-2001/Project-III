ca1d3054e141635a38f76d1788e9b9280c483938
james-2001 <james.a.may.2001@gmail.com>
2022-01-04 12:20:29 +0000

    bb rb comparison graph

--- bb_vs_rb.r
+++ bb_vs_rb.r
@@ -13,12 +13,27 @@ bayesian_b <- function(R) {
     return(t)
 }
 
-n <- 10
-r <- seq(500, 5000, length.out = n)
-bb_cis <- data.frame("r" = r, "lower" = rep(0, n),
-                     "upper" = rep(0, n))
-
-for (i in 1:n) {
-    bb_cis$lower[i] <- quantile(bayesian_b(r[i]), 0.025)
-    bb_cis$upper[i] <- quantile(bayesian_b(r[i]), 0.975)
+theta_hat <- function(data, i){
+    mean(data[i])
 }
+
+bb_t <- bayesian_b(5000)
+rb <- boot(data, theta_hat, 5000)
+
+par(mfrow = c(1,2))
+hist(bb_t)
+abline(v = quantile(bb_t, c(0.025, 0.5, 0.975)),
+       col = "red")
+hist(rb$t)
+abline(v = quantile(rb$t, c(0.025, 0.5, 0.975)),
+       col = "red")
+
+# n <- 3
+# r <- seq(500, 5000, length.out = n)
+# bb_cis <- data.frame("r" = r, "lower" = rep(0, n),
+#                      "upper" = rep(0, n))
+
+# for (i in 1:n) {
+#     bb_cis$lower[i] <- quantile(bayesian_b(r[i]), 0.025)
+#     bb_cis$upper[i] <- quantile(bayesian_b(r[i]), 0.975)
+# }
