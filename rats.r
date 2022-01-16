40fb95d2aac512fabbcc59811d23779dfc8ab410
james-2001 <james.a.may.2001@gmail.com>
2022-01-16 16:21:54 +0000

    wrong bb

--- rats.r
+++ rats.r
@@ -1,2 +1,13 @@
 library(boot)
+library(mc2d)
 
+R <- 5000
+
+model <- lm(log(surv) ~ dose, data = survival)
+n <- nrow(survival)
+
+t <- rep(0, R)
+for (i in 1:R) {
+    pi <- rdirichlet(1, rep(1,n))
+    t[i] <- sum(pi * model$residuals**2) / (n - 2)
+}
