a591ccfb78e788740823932ceb0cc56824993bb8
james-2001 <james.a.may.2001@gmail.com>
2022-01-25 21:23:03 +0000

    bb for median

--- bb.r
+++ bb.r
@@ -2,18 +2,37 @@ library(mc2d)
 library(boot)
 set.seed(50)
 
-data <- rnorm(500)
-bb <- function(data, R) {
+n <- 500
+
+data <- rnorm(n)
+bb_mean <- function(data, R) {
     t <- rep(0, R)
     for (i in 1:R) {
-        dir_samp <- mc2d::rdirichlet(1, rep(1, 500))
+        dir_samp <- mc2d::rdirichlet(1, rep(1, n))
         t[i] <- weighted.mean(data, dir_samp)
     }
     return(t)
 }
 
-theta_hat <- function(data, i){
+
+bb_quant <- function(data, R, q) {
+    t <- rep(0, R)
+    dir_samp <- mc2d::rdirichlet(R, rep(1, n))
+    for (i in 1:R) {
+        cum_sum <- 0
+        j <- 1
+        while (cum_sum <= q) {
+            cum_sum <- cum_sum + dir_samp[i, j]
+            j <- j + 1
+        }
+        t[i] <- data[i]
+    }
+    return(t)
+}
+
+
+mean_hat <- function(data, i){
     mean(data[i])
 }
-data_bb <- bb(data, 5000)
-data_b <- boot(data, theta_hat, 5000)
+data_bb_mean <- bb_mean(data, 5000)
+data_b <- boot(data, mean_hat, 5000)
