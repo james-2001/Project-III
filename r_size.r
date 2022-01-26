df772fb7507a9c80aaa7d4b601ab4221e25ac67b
james-2001 <james.a.may.2001@gmail.com>
2022-01-26 12:03:07 +0000

    bb r size

--- r_size.r
+++ r_size.r
@@ -7,17 +7,43 @@ data <- rnorm(100)
 boot_mean <- function(data, i) {
     mean(data[i])
 }
+
+bb_mean <- function(data, R) {
+    t <- rep(0, R)
+    for (i in 1:R) {
+        dir_samp <- mc2d::rdirichlet(1, rep(1, 100))
+        t[i] <- weighted.mean(data, dir_samp)
+    }
+    return(t)
+}
+
+
 r <- seq(from = 10, to = 8000, by = 50)
 n <- length(r)
-bounds <- data.frame("2.5%" = rep(0, n),
+b_bounds <- data.frame("2.5%" = rep(0, n),
                      "97.5%" = rep(0, n))
 
 for (i in 1:n) {
     b <- boot(data, boot_mean, R = r[i])
-    bounds[i, ] <- quantile(b$t, c(0.025, 0.975))
+    b_bounds[i, ] <- quantile(b$t, c(0.025, 0.975))
 }
 
-plot_lim <- c(min(bounds$X2.5.), max(bounds$X97.5.))
+plot_lim <- c(min(b_bounds$X2.5.), max(b_bounds$X97.5.))
 
-plot(bounds$X2.5., col = "red", ylim = plot_lim, type = "l")
-lines(bounds$X97.5., col = "green")
+
+#par(mfrow = c(2,0))
+# plot(b_bounds$X2.5., col = "red", ylim = plot_lim, type = "l")
+# lines(b_bounds$X97.5., col = "green")
+
+bb_bounds <- data.frame("2.5%" = rep(0, n),
+                        "50%" = rep(0, n),
+                        "97.5%" = rep(0, n))
+
+for (i in 1:n) {
+    b <- bb_mean(data, r[i])
+    bb_bounds[i, ] <- quantile(b, c(0.025, 0.5, 0.975))
+}
+
+plot(bb_bounds$X2.5., col = "red", ylim = plot_lim, type = "l")
+lines(bb_bounds$X97.5., col = "green")
+lines(bb_bounds$X50)
