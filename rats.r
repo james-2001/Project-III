5961bd2ee85ac81506e3e01eb8d85c0080313aff
james-2001 <james.a.may.2001@gmail.com>
2022-02-07 14:12:21 +0000

    bb vs boot distribution comparison

--- rats.r
+++ rats.r
@@ -29,3 +29,24 @@ abline(v = quantile(t, c(0.025, 0.975)), col = "red")
 abline(v = mean(t), col = "blue")
 dev.off()
 
+
+theta_hat <- function(data, i){
+    d <- data[i,]
+    fit <- lm(log(surv)~dose, data = d)
+    v <- summary(fit)$sigma**2
+    }
+
+eb_t <- boot(survival, theta_hat, R = 5000)
+
+dev.new()
+pdf("cd_compare.pdf")
+plot(ecdf(t),
+     main = NULL,
+     cex.lab = 1.5)
+lines(ecdf(eb_t$t), col = "red")
+legend(4.5,0.2,
+       legend = c("Bayesian Bootstrap", "Efron's Bootstrap"),
+       col = c("black", "red"),
+       lty = 1, box.lty = 0,
+       cex = 1.5)
+dev.off()
