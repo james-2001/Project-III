286d8db21fea0d8bb515974eace034de5dd60765
james-2001 <james.a.may.2001@gmail.com>
2022-02-15 13:05:49 +0000

    outlier hist + cdf

--- outliers.r
+++ outliers.r
@@ -9,9 +9,10 @@ r <- 5000
 
 b_mean <- boot(data, function(d, i) mean(d[i]), R = r)
 
+weights <- rdirichlet(r, rep(1,n))
 bb_mean <- rep(0, r)
 for (i in 1:r) {
-    bb_mean[i] <- sum(rdirichlet(1, rep(1, n)) * data)
+    bb_mean[i] <- sum(weights[i] * data)
 }
 
 par(mfrow = c(2, 2))
