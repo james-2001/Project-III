51bee40171a4092f352cce06f3a560caeeffe59f
james-2001 <james.a.may.2001@gmail.com>
2022-02-18 18:16:31 +0000

    not working

--- outliers.r
+++ outliers.r
@@ -2,23 +2,22 @@ library(boot)
 library(mc2d)
 set.seed(50)
 
-n <- 20
+n <- 10
 sample <- rexp(n - 1)
-data <- append(sample, 25)
+data <- append(sample, 10)
 r <- 5000
 
-b_mean <- boot(data, function(d, i) mean(d[i]), R = r)
+b_mean <- boot(sample, function(d, i) mean(d[i]), R = r)
 
-weights <- rdirichlet(r, rep(1,n))
+weights <- rdirichlet(r, rep(1, n))
 bb_mean <- rep(0, r)
 for (i in 1:r) {
-    bb_mean[i] <- sum(weights[i] * data)
+    bb_mean[i] <- sum(weights[i] * sample)
 }
 
 par(mfrow = c(2, 2))
 hist(b_mean$t)
-abline(v = c(mean(sample), mean(data)))
 hist(bb_mean)
 
 plot(ecdf(b_mean$t))
-plot(ecdf(bb_mean))
+lines(ecdf(bb_mean), col = "red")
