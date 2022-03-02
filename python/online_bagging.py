2f9f8b2b9370b008cfe8ef359828c3c1773461bf
james-2001 <james.a.may.2001@gmail.com>
2022-03-02 15:10:20 +0000

    online baggin alg

--- python/online_bagging.py
+++ python/online_bagging.py
@@ -1,3 +1,18 @@
 from sklearn.datasets import make_classification
+from numpy.random import poisson
 
 data, target = make_classification()
+full = list(zip(data,target))
+r=25
+
+bagged_data = [{"data":[], "target":[]} for _ in range(r)]
+def process(new):
+    d,t = new
+    for i in range(r):
+        k = poisson()
+        bagged_data[i]["data"]+=k*[d]
+        bagged_data[i]["target"] += k*[t]
+for point in full:
+    process(point)
+
+print(bagged_data[0])
