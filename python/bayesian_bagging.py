760a2566d96832f13fc41f953773361a633f6a9e
james-2001 <james.a.may.2001@gmail.com>
2022-03-02 09:35:06 +0000

    broken

--- python/bayesian_bagging.py
+++ python/bayesian_bagging.py
@@ -1,20 +1,12 @@
 from sklearn import tree
 from numpy import array
-from sklearn.datasets import load_iris
-from bayesian_boot import generate_samples
-from scipy.stats import mode
+from numpy.random import choice, dirichlet
+from sklearn.datasets import make_classification
 
-iris = load_iris()
-data, targets = iris.data, iris.target
+data, target = make_classification()
+r=50
+n=len(target)
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(data, targets)
+bb_samples = array([array(list(zip(data,target)), dtype=object)[list(choice(range(n), size = n, p = dirichlet([1]*n)))] for _ in range(25)])
+bagged_trees = [tree.DecisionTreeClassifier().fit(sample[:,0],sample[:,1]) for sample in bb_samples]
 
-resamples = generate_samples(data, 25)
-bagging = [tree.DecisionTreeClassifier().fit(boot, targets)
-           for boot in resamples]
-
-bagging_test = array([phi.predict(data) for phi in bagging])
-bagging_test = mode(bagging_test).mode.flatten()
-bagging_misclass = sum([bagging_test[i]!=targets[i] for i in range(len(targets))])
-print(bagging_misclass/len(targets))
