dcccef095111dff597190930b3cd4bc6a6d3a66c
james-2001 <james.a.may.2001@gmail.com>
2022-03-02 10:13:37 +0000

    bagging working

--- python/bayesian_bagging.py
+++ python/bayesian_bagging.py
@@ -1,12 +1,19 @@
 from sklearn import tree
-from numpy import array
+from numpy import array, append
 from numpy.random import choice, dirichlet
 from sklearn.datasets import make_classification
 
 data, target = make_classification()
-r=50
+r=2
 n=len(target)
 
-bb_samples = array([array(list(zip(data,target)), dtype=object)[list(choice(range(n), size = n, p = dirichlet([1]*n)))] for _ in range(25)])
-bagged_trees = [tree.DecisionTreeClassifier().fit(sample[:,0],sample[:,1]) for sample in bb_samples]
+bb_data  = []
+bb_target = []
+for i in range(r):
+    weights = dirichlet([1]*n)
+    sample_indexs = choice(range(n), size = n, p=weights)    
+    bb_data.append(array(data)[sample_indexs])
+    bb_target.append(array(target)[sample_indexs])
+
+bagged_trees = [tree.DecisionTreeClassifier().fit(bb_data[i], bb_target[i]) for i in range(r)]
 
