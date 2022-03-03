39031a15de0c287c87ddaae551320e5420f46192
james-2001 <james.a.may.2001@gmail.com>
2022-03-03 21:01:16 +0000

    comparison across datasets

--- python/bagging_improvement.py
+++ python/bagging_improvement.py
@@ -1,9 +1,14 @@
 from bagging_rate import bagging_rate
-from sklearn.datasets import make_classification, load_iris
-from numpy import array, mean
+from sklearn.datasets import load_wine, make_classification, load_iris, load_digits
+from numpy import array, mean, append
 
-iris = load_iris()
-data, target = iris.data, iris.target
-
-improvements = array([bagging_rate(data, target) for _ in range(100)])
-print(mean(improvements, axis=0))
+for loader in [load_iris, load_digits, load_wine, make_classification]:
+    dataset = loader()
+    if loader == make_classification:
+        data, target = dataset
+    else:
+        data, target = dataset.data, dataset.target
+    improvements = array([bagging_rate(data, target) for _ in range(100)])
+    improvements = mean(improvements, axis=0)
+    improvements = append(improvements,100*(improvements[0]-improvements[1])/improvements[1])
+    print(f"{loader.__name__}:{improvements}")
