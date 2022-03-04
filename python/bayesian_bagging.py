12a95a5a1cbdcccb2410b71de248652bf88e98cc
james-2001 <james.a.may.2001@gmail.com>
2022-03-04 17:09:03 +0000

    bb class; broke

--- python/bayesian_bagging.py
+++ python/bayesian_bagging.py
@@ -1,21 +1,52 @@
-from sklearn import tree
-from numpy import array, append
-from numpy.random import choice, dirichlet
+from numpy import array_split, array, delete
+from sklearn.tree import DecisionTreeClassifier
+from numpy.random import dirichlet
+from statistics import mode 
 from sklearn.datasets import make_classification
-from sklearn.ensemble import BaggingClassifier
 
-data, target = make_classification()
-r=2
-n=len(target)
+class BayesianBaggingClassifier:
+    def __init__(self, n_estimators) -> None:
+        self.data = []
+        self.target = []
+        self.r = n_estimators
+        self.estimators = []
+        self.n = len(self.target)
 
-# bb_data  = []
-# bb_target = []
-# for i in range(r):
-#     weights = dirichlet([1]*n)
-#     sample_indexs = choice(range(n), size = n, p=weights)    
-#     bb_data.append(array(data)[sample_indexs])
-#     bb_target.append(array(target)[sample_indexs])
+    def fit(self, data, target):
+        self.estimators = self.local_fit(data, target)
+        self.data = data
+        self.target = target
 
-# bagged_trees = [tree.DecisionTreeClassifier().fit(bb_data[i], bb_target[i]) for i in range(r)]
+    def local_fit(self, data, target):
+        self.n = len(target)
+        bag = [DecisionTreeClassifier().fit(data, target, sample_weight=weight)
+               for weight in dirichlet([1]*self.n, self.r)]
+        return bag
 
-bag = BaggingClassifier(n_estimators=25).fit(data,target,sample_weight=dirichlet([1]*n)) #do you want new dir samples each time?
+    def bb_predict(self, new_data, bag = None):
+        if bag is None:
+            bag = self.estimators
+        predictions = [phi.predict(new_data) for phi in bag]
+        return [mode([phi[i] for phi in predictions]) for i in range(len(new_data))]
+
+    def cross_val(self, cv = 10):
+        partition = array_split(array(list(zip(self.data, self.target))), cv)
+        scores = []
+        for i in range(cv):
+            learning_data, learning_targets = zip(*self.flatten(delete(partition, i)))
+            training_data, training_targets = zip(*partition[i])
+            bag = self.fit(learning_data, learning_targets, local = True)
+            cv_predict = self.bb_predict(training_data, bag)
+            scores.append(sum([cv_predict[i]!=training_targets[i] for i in len(training_targets)]))
+        return sum(scores)/cv
+            
+
+    def flatten(self, t:list):
+        return [item for sublist in t for item in sublist]
+
+
+if __name__ == "__main__":
+    d,t = make_classification()
+    bag = BayesianBaggingClassifier(n_estimators=25)
+    bag.fit(d,t)
+    print(bag.cross_val())
