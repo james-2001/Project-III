ac95c4ea4a61004431c2926d745e8ce42ee59746
james-2001 <james.a.may.2001@gmail.com>
2022-03-05 22:36:20 +0000

    cross val: working?

--- python/bayesian_bagging.py
+++ python/bayesian_bagging.py
@@ -1,8 +1,9 @@
-from numpy import array_split, array, delete
+from numpy import array_split, array, delete, partition
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from numpy.random import dirichlet
 from statistics import mode 
-from sklearn.datasets import make_classification
+from sklearn.datasets import make_classification, load_digits
 
 class BayesianBaggingClassifier:
     def __init__(self, n_estimators) -> None:
@@ -16,11 +17,12 @@ class BayesianBaggingClassifier:
         self.estimators = self.local_fit(data, target)
         self.data = data
         self.target = target
+        self.n = len(self.target)
 
     def local_fit(self, data, target):
-        self.n = len(target)
+        loc_n = len(target)
         bag = [DecisionTreeClassifier().fit(data, target, sample_weight=weight)
-               for weight in dirichlet([1]*self.n, self.r)]
+               for weight in dirichlet([1]*loc_n, self.r)]
         return bag
 
     def bb_predict(self, new_data, bag = None):
@@ -29,24 +31,24 @@ class BayesianBaggingClassifier:
         predictions = [phi.predict(new_data) for phi in bag]
         return [mode([phi[i] for phi in predictions]) for i in range(len(new_data))]
 
-    def cross_val(self, cv = 10):
-        partition = array_split(array(list(zip(self.data, self.target))), cv)
+    def cross_val(self, data, target, cv = 10):
         scores = []
-        for i in range(cv):
-            learning_data, learning_targets = zip(*self.flatten(delete(partition, i)))
-            training_data, training_targets = zip(*partition[i])
-            bag = self.fit(learning_data, learning_targets, local = True)
+        n = len(target)
+        for i in range(n//cv):
+            learning_data = list(data)[:i*cv]+list(data)[min(cv*(i+1),n):]
+            learning_targets = list(target)[:i*cv]+list(target)[min(cv*(i+1),n):]
+            training_data = data[i*cv:min((i+1)*cv, n)]
+            training_targets = target[i*cv:min((i+1)*cv, n)]
+            bag = self.local_fit(data=learning_data, target=learning_targets)
             cv_predict = self.bb_predict(training_data, bag)
-            scores.append(sum([cv_predict[i]!=training_targets[i] for i in len(training_targets)]))
+            scores.append(sum([cv_predict[i]!=training_targets[i] for i in range(len(training_targets))])/len(training_targets))
         return sum(scores)/cv
             
 
-    def flatten(self, t:list):
-        return [item for sublist in t for item in sublist]
-
-
 if __name__ == "__main__":
-    d,t = make_classification()
+    load= load_digits()
+    d,t = load.data, load.target
+    print(len(t))
     bag = BayesianBaggingClassifier(n_estimators=25)
-    bag.fit(d,t)
-    print(bag.cross_val())
+    #bag.fit(d,t)
+    print(bag.cross_val(d,t))
