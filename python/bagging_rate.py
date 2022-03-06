9b4c2f7c12d50b0015bd976f5025e42d6bf6c6eb
james-2001 <james.a.may.2001@gmail.com>
2022-03-06 09:47:05 +0000

    cross val comparison

--- python/bagging_rate.py
+++ python/bagging_rate.py
@@ -1,14 +1,11 @@
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
-from sklearn.datasets import make_classification
 
 def bagging_rate(data, target):
     bag = BaggingClassifier(n_estimators=25)
-    bag = bag.fit(data, target)
 
     clf = DecisionTreeClassifier()
-    clf = clf.fit(data, target)
 
-    return  [cross_val_score(method, data, target, cv = 10).mean()
+    return  [1-cross_val_score(method, data, target, cv = 10).mean()
              for method in (bag, clf)]
