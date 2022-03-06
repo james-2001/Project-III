675f2d9fc751e88fa5bd11779b6cecb64fbc57e3
james-2001 <james.a.may.2001@gmail.com>
2022-03-06 09:46:32 +0000

    bagging vs not

--- python/bagging_improvement.py
+++ python/bagging_improvement.py
@@ -10,5 +10,5 @@ for loader in [load_iris, load_digits, load_wine, make_classification]:
         data, target = dataset.data, dataset.target
     improvements = array([bagging_rate(data, target) for _ in range(100)])
     improvements = mean(improvements, axis=0)
-    improvements = append(improvements,100*(improvements[0]-improvements[1])/improvements[1])
-    print(f"{loader.__name__}:{improvements}")
+    improvements = append(improvements,100*(1-improvements[0]/improvements[1]))
+    print(f"{loader.__name__}:{improvements}%")
