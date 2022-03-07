96d5e25c9c34b27c1e8af48df010c1dcd4e7f946
james-2001 <james.a.may.2001@gmail.com>
2022-03-07 20:21:00 +0000

    linting

--- python/online_bagging.py
+++ python/online_bagging.py
@@ -2,15 +2,17 @@ from sklearn.datasets import make_classification
 from numpy.random import poisson
 
 data, target = make_classification()
-full = list(zip(data,target))
-r=25
+full = list(zip(data, target))
+r = 25
 
-bagged_data = [{"data":[], "target":[]} for _ in range(r)]
+bagged_data = [{"data": [], "target": []} for _ in range(r)]
+
+
 def process(new):
-    d,t = new
+    d, t = new
     for i in range(r):
         k = poisson()
-        bagged_data[i]["data"]+=k*[d]
+        bagged_data[i]["data"] += k*[d]
         bagged_data[i]["target"] += k*[t]
 
 
