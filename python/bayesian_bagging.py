18b496358c19208ca83aa6009011fb3751b94cfa
james-2001 <james.a.may.2001@gmail.com>
2022-03-07 17:11:35 +0000

    linting

--- python/bayesian_bagging.py
+++ python/bayesian_bagging.py
@@ -1,7 +1,8 @@
 from sklearn.tree import DecisionTreeClassifier
 from numpy.random import dirichlet, exponential
-from statistics import mode 
+from statistics import mode
 from sklearn.datasets import make_classification
+
 
 class BayesianBaggingClassifier:
     def __init__(self, n_estimators) -> None:
@@ -25,12 +26,14 @@ class BayesianBaggingClassifier:
 
     def online_fit(self, data, target):
         self.n = len(target)
-        bagged_data = [{"data":[], "target":[], "weight":[]} for _ in range(self.r)]
+        bagged_data = [{"data": [], "target": [], "weight": []}
+                       for _ in range(self.r)]
         for i in range(self.n):
             bagged_data = self.online_process(data[i], target[i], bagged_data)
-        self.estimators =  [DecisionTreeClassifier().fit(learn["data"], learn["target"], sample_weight=learn["weight"])
-                            for learn in bagged_data]
-    
+        self.estimators = [DecisionTreeClassifier().fit(learn["data"],
+                           learn["target"], sample_weight=learn["weight"])
+                           for learn in bagged_data]
+
     def online_process(self, point, target, bagged_data):
         for i in range(self.r):
             k = exponential()
@@ -39,37 +42,39 @@ class BayesianBaggingClassifier:
             bagged_data[i]["weight"] += [k]
         return bagged_data
 
-
-
-    def bb_predict(self, new_data, bag = None):
+    def bb_predict(self, new_data, bag=None):
         if bag is None:
             bag = self.estimators
         predictions = [phi.predict(new_data) for phi in bag]
-        return [mode([phi[i] for phi in predictions]) for i in range(len(new_data))]
+        return [mode([phi[i] for phi in predictions])
+                for i in range(len(new_data))]
 
-    def cross_val(self, data, target, cv = 10):
+    def cross_val(self, data, target, cv=10):
         scores = []
         n = len(target)
         for i in range(n//cv):
-            learning_data = list(data)[:i*cv]+list(data)[min(cv*(i+1),n):]
-            learning_targets = list(target)[:i*cv]+list(target)[min(cv*(i+1),n):]
+            learning_data = list(data)[:i*cv]+list(data)[min(cv*(i+1), n):]
+            learning_targets = list(target)[:i*cv]+list(target)[min(cv*(i+1),
+                                                                    n):]
             training_data = data[i*cv:min((i+1)*cv, n)]
             training_targets = target[i*cv:min((i+1)*cv, n)]
             bag = self.local_fit(data=learning_data, target=learning_targets)
             cv_predict = self.bb_predict(training_data, bag)
-            scores.append(sum([cv_predict[i]!=training_targets[i] for i in range(len(training_targets))])/len(training_targets))
+            scores.append(sum([cv_predict[i] != training_targets[i]
+                               for i in range(len(training_targets))])/len(
+                               training_targets))
         return sum(scores)/cv
-    
+
     def score(self, t_data, t_target):
-        n=len(t_target)
+        n = len(t_target)
         preds = self.bb_predict(t_data)
-        return sum([preds[i]!=t_target[i] for i in range(n)])/n
-            
+        return sum([preds[i] != t_target[i] for i in range(n)])/n
+
 
 if __name__ == "__main__":
-    d,t = make_classification()
+    d, t = make_classification()
     dl, tl = d[:30], t[:30]
-    dt,tt = d[30:], t[30:]
+    dt, tt = d[30:], t[30:]
     bag = BayesianBaggingClassifier(n_estimators=25)
-    bag.online_fit(dl,tl)
-    print(bag.score(dt,tt))
+    bag.online_fit(dl, tl)
+    print(bag.score(dt, tt))
