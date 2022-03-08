596bc7aa4e24f037e46b6bc7091cb6525e5f4b6d
james-2001 <james.a.may.2001@gmail.com>
2022-03-08 11:06:14 +0000

    score method

--- python/online_bagging.py
+++ python/online_bagging.py
@@ -1,22 +1,38 @@
-from sklearn.datasets import make_classification
 from numpy.random import poisson
-
-data, target = make_classification()
-full = list(zip(data, target))
-r = 25
-
-bagged_data = [{"data": [], "target": []} for _ in range(r)]
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_classification
+from statistics import mode
 
 
-def process(new):
-    d, t = new
-    for i in range(r):
-        k = poisson()
-        bagged_data[i]["data"] += k*[d]
-        bagged_data[i]["target"] += k*[t]
+class OnlineBaggingClassifier:
+    def __init__(self, n_estimators=25) -> None:
+        self.r = n_estimators
+        self.estimators = []
+        self.bagged_data = [{"Data": [], "Target": []}*n_estimators]
+
+    def online_bag(self, data, target):
+        for i in range(len(target)):
+            self.process(data[i], target[i])
+        self.estimators = [DecisionTreeClassifier().fit(x["data"], x["target"])
+                           for x in self.bagged_data]
+
+    def process(self, d, t):
+        for i in range(self.r):
+            k = poisson()
+            self.bagged_data[i]["data"] += k*[d]
+            self.bagged_data[i]["target"] += k*[t]
+
+    def ob_predict(self, d):
+        predictions = [phi.predict(d) for phi in self.estimators]
+        return [mode([phi[i] for phi in predictions])
+                for i in range(len(d))]
+
+    def score(self, d, t):
+        n = len(t)
+        predictions = self.ob_predict(d)
+        return sum([predictions[i] != t[i] for i in range(n)])/n
 
 
-for point in full:
-    process(point)
-
-print(bagged_data[0])
+if __name__ == "__main__":
+    d, t = make_classification()
+    online_bag = OnlineBaggingClassifier()
